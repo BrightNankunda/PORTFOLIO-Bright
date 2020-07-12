8896d016a5bbd51a35094d859a9feb97d55ed5c6
BrightNankunda <67958069+BrightNankunda@users.noreply.github.com>
2020-07-12 11:28:44 +0300

    Add files via upload

---
+++ experience.js
@@ -0,0 +1,14 @@
+function experience () {
+	var div = document.getElementById("main");
+	div.innerHTML = "I worked at KAFACE INVESTEMENTS since JUNE 2019. <br>I am now working at JAST TECH";
+}
+function Education () {
+	var div = document.getElementById("main");
+	div.style.width = "70%";
+	div.innerHTML = "I attended Midland High School Kawempe for my A'level. <br> Makerere University for my Bachelors.";
+}
+function contact () {
+	var div = document.getElementById("main");
+	div.style.width = "70%";
+	div.innerHTML = "070-863-2694<br>nbright597@gmail.com";
+}
